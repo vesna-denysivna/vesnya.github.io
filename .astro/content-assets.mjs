96f2e251fab965407aca15923c473c711206913e
Vesna Denysivna <nero.coreboot@disroot.org>
2025-01-30 02:56:36 +0200

    new file:   .astro/collections/blog.schema.json

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,9 @@
+
+import __ASTRO_IMAGE_IMPORT_qajyw from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-1.md";
+import __ASTRO_IMAGE_IMPORT_qaryT from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-2.md";
+import __ASTRO_IMAGE_IMPORT_qazzh from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-3.md";
+import __ASTRO_IMAGE_IMPORT_qaHzE from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-4.md";
+import __ASTRO_IMAGE_IMPORT_qaPA2 from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-5.md";
+import __ASTRO_IMAGE_IMPORT_qaXAp from "../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-6.md";
+export default new Map([["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-1.md", __ASTRO_IMAGE_IMPORT_qajyw], ["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-2.md", __ASTRO_IMAGE_IMPORT_qaryT], ["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-3.md", __ASTRO_IMAGE_IMPORT_qazzh], ["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-4.md", __ASTRO_IMAGE_IMPORT_qaHzE], ["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-5.md", __ASTRO_IMAGE_IMPORT_qaPA2], ["../../assets/astro.jpeg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fblogpost-6.md", __ASTRO_IMAGE_IMPORT_qaXAp]]);
+		
